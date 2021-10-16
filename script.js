4149fb9d38558471994076eefb4221d8c6bbe86f
chinmay <chinmaybs89@gmail.com>
2021-10-16 18:26:27 +0530

    complete simple google sign up

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
+'use strict';
 
+
+// QUERY SELECTOR TO SELECT BUTTONS AND CONTAINER IN A WEB PAGE
 const googleSignInButton=document.querySelector('.g-signin2');
+const googleSignOutButton=document.querySelector('.sign-out');
+const loginScreen=document.querySelector('.container--1');
+const logoutScreen=document.querySelector('.container--2')
 
 
+const userName=document.querySelector('.name');
+const email=document.querySelector('.email');
+const image=document.querySelector('.image');
+
+
+logoutScreen.style.display='none';
+
+
+// BELOW CODE EXECUTES WHEN LOGIN BUTTON PRESSED
 function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-
-  // location.href="album.html";
-
+  const profile = googleUser.getBasicProfile();
+  loginScreen.style.display='none';
+  logoutScreen.style.display='flex';
+  userName.textContent=profile.getName();
+  email.textContent=profile.getEmail();
+  image.src=profile.getImageUrl()
 }
 
 
+// BELOW FUNCTION EXECUTES WHEN SIGNOUT BUTTON IS PRESSES
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    // console.log('User signed out.');
+    loginScreen.style.display='flex';
+    logoutScreen.style.display='none';
+  });
+}
 
-window.onLoadCallback = function(){
+
+// ADDING EVENT LISTENER FOR SIGNOUT BUTTON
+googleSignOutButton.addEventListener('click',signOut);   
+
+
+// CALLING INIT FUNCTION AFTER LOADING GOOGLE API(defer async loading)
+window.onLoadCallback = function(){            
   gapi.auth2.init({
       client_id: '15995344385-4bht6p5l1m00icu35emfnrbd6pug7uld.apps.googleusercontent.com'
     });
